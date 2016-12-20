95319581e3e566cb5d330bdd98945232a49e762f
Prakash Aryal <aryalprakas@gmail.com>
2016-12-20 23:51:41 +0545

    merged

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,5 @@
-export const API_URL = 'http://localhost:3000';
+//export const API_URL = 'http://localhost:3000';
+export const API_URL = '';
 
 export const GET_MAIN_CATEGORIES = 'GET_MAIN_CATEGORIES';
 export const GET_STORES_LIST = 'GET_STORES_LIST';
