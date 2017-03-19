14dab19c8620d552aeee44932d348092d1b58545
shrestha-bikash <bikash_stha637@hotmail.com>
2017-03-19 10:31:26 +0545

    changes in user notification page

--- src/components/profile/UserNotifications.js
+++ src/components/profile/UserNotifications.js
@@ -7,10 +7,19 @@ import { Link } from 'react-router';
 import Deals from '../Deals';
 import ProfileSideBar from './ProfileSideBar';
 
-const style = {
-  width: 80,
-  height: 'auto'
+const styles = {
+  imgStyle: {
+    width: 80,
+    height: 'auto'
+  },
+
+  p: {
+    fontSize: 12,
+    color: '#aaa'
+  }
+
 };
+
 class UserNotifications extends Component {
   render() {
     return(
@@ -20,12 +29,13 @@ class UserNotifications extends Component {
           <div className="media" style={{marginTop: 15}}>
             <div className="media-left">
               <Link to="#">
-                <img style={style} className="media-object" src={require("../../../img/store.png")} alt="..."/>
+                <img style={styles.imgStyle} className="media-object" src={require("../../../img/store.png")} alt="..."/>
               </Link>
             </div>
             <div className="media-body" >
               <h4 className="media-heading" style={{marginTop: 5}}>Wallmart has ELectronics Sales</h4>
-              ...
+              <p style={styles.p}>2 days ago</p>
+              <p>lorem epsium garcia sochen oasios gipum thisum dishum</p>
             </div>
           </div>
         </div>
