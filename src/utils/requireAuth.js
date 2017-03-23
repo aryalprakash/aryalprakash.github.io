49bd78628b29c1b3653377d9a44b16b8c6d789d0
shrestha-bikash <bikash_stha637@hotmail.com>
2017-03-23 12:27:47 +0545

    added requireAuth to check the whether the user is logged in or not

---
+++ src/utils/requireAuth.js
@@ -0,0 +1,49 @@
+/**
+ * Created by bikash on 3/23/17.
+ */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function(ComposedComponent) {
+  class Authenticate extends Component {
+
+    componentWillReceiveProps(nextProps){
+
+      if(this.props.loggedIn !== nextProps.loggedIn){
+        console.log("requireAuth props",nextProps.loggedIn);
+
+        if(nextProps.loggedIn.status_code !== 200){
+          this.context.router.push('/login');
+        }
+
+        if(nextProps.loggedIn.status_code === 200){
+          this.context.router.push('/');
+        }
+      }
+
+    }
+
+    render(){
+      return (
+        <ComposedComponent {...this.props} />
+      );
+    }
+  }
+
+  Authenticate.propTypes = {
+    loggedIn: React.PropTypes.object.isRequired
+  };
+
+  Authenticate.contextTypes = {
+    router: React.PropTypes.object.isRequired
+  };
+
+
+  function mapStateToProps(state) {
+    return {
+      loggedIn: state.auth.loggedIn
+    }
+  }
+
+  return connect(mapStateToProps)(Authenticate);
+}
