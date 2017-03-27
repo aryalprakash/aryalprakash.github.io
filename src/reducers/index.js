f90f71bba3b295d6a4ca6cc708a6b25596da199a
shrestha-bikash <bikash_stha637@hotmail.com>
2017-03-27 16:32:32 +0545

    added user reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,16 @@
 import { combineReducers } from 'redux';
-import InfiniaStores from './infiniaReducer';
+import infiniaStores from './infiniaReducer';
 import cartReducer from './cartReducer';
 import authReducer from './authReducer';
 import storeReducer from './storeReducer';
 import productReducer from './productReducer';
 import shippingReducer from './shippingReducer';
+import userReducer from './userReducer';
 
 export default combineReducers({
   auth: authReducer,
-  InfiniaStores: InfiniaStores,
+  InfiniaStores: infiniaStores,
+  user: userReducer,
   cart: cartReducer,
   stores: storeReducer,
   products: productReducer,
