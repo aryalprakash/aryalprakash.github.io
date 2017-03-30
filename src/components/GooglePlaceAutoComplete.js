60aa77a8642df2639abf505073598ebca1e989e8
shrestha-bikash <bikash_stha637@hotmail.com>
2017-03-30 16:16:13 +0545

    component for search places using goole place api

---
+++ src/components/GooglePlaceAutoComplete.js
@@ -0,0 +1,57 @@
+/**
+ * Created by bikash on 3/30/17.
+ */
+import React, { Component } from 'react';
+
+class PlaceAutoComplete extends Component {
+
+  state = {
+    place: ''
+  };
+
+  handleChange = (e) =>{
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    this.autoComplete();
+  };
+
+  autoComplete = (e) => {
+    var autocomplete = new google.maps.places.Autocomplete(document.getElementById('location_search'), {types:[ 'address']});
+
+    autocomplete.addListener('place_changed', e => {
+      var place = autocomplete.getPlace();
+
+      this.props.updatePlace(place);
+      this.setState({place: place.formatted_address});
+    });
+  };
+
+  render() {
+    return(
+      <div className="form-group">
+        <input style={this.props.style}
+               width={this.props.width}
+               type="text"
+               id="location_search"
+               className="form-control"
+               onChange={this.handleChange}
+               value={this.state.place}
+               name='place'
+               placeholder="Search location..."
+        />
+      </div>
+    )
+  }
+}
+
+PlaceAutoComplete.propTypes = {
+  updatePlace: React.PropTypes.func.isRequired
+};
+
+PlaceAutoComplete.defaultProps = {
+  style: {},
+  width: 'auto',
+};
+
+export default PlaceAutoComplete;
