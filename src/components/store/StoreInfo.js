dc63418bb47d456377139590266c3e4d686bfc86
shrestha-bikash <bikash_stha637@hotmail.com>
2017-04-02 11:07:42 +0545

    added StoreAboutUs component

--- src/components/store/StoreInfo.js
+++ src/components/store/StoreInfo.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 
 import Gmap from '../maps';
 import StoreNavBar from './StoreNavBar';
+import StoreAboutUs from './StoreAboutUs';
 import {getStoreDetails} from '../../actions/storeActions';
 
 
@@ -22,45 +23,13 @@ class StoreInfo extends Component {
         {
           storeDetails.length>0 ?
             <div className="">
+
               <div className="col-md-4">
                 <h4>About Us</h4>
                 <div className="line"></div>
-                <div className="store-profile-name">{storeDetails[0].display_name}</div>
-                <div className="store-profile-address">{storeDetails[0].street}, {storeDetails[0].state}</div>
-
-                <div className="card">
-                  Store Description
-                  <div className="store-profile-content">{storeDetails[0].description}</div>
-                </div>
-
-                <div className="card">
-                  Opening Hours
-                  <div className="store-profile-content">{storeDetails[0].opening_hours}</div>
-                  Closing Hours
-                  <div className="store-profile-content">{storeDetails[0].closing_hours}</div>
-                </div>
-                <div className="card">
-                  Delivery Areas
-                  <div className="store-profile-content">
-                    {storeDetails[0].delivery_area.map(data => data+ " ")}
-                  </div>
-                </div>
-                <div className="card">
-                  Minimum Order
-                  <div className="store-profile-content">{storeDetails[0].minimum_buy} AED</div>
-                </div>
-                <div className="card">
-                  Categories Served
-                  <div className="store-profile-content">
-                    {
-                      storeDetails[0].categorysecond.map((cat)=>
-                        (cat.category)+" "
-                      )
-                    }
-                  </div>
-                </div>
-
+                <StoreAboutUs storeDetails={storeDetails[0]}/>
               </div>
+
               <div className="store-map col-md-8">
                 <h4>Store Location</h4>
                 <div className="line"></div>
