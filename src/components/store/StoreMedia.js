3c99628acebecc9901fd9ecb84a134df90b1b4d4
shrestha-bikash <bikash_stha637@hotmail.com>
2017-05-10 14:35:30 +0545

    changes in grid classes

--- src/components/store/StoreMedia.js
+++ src/components/store/StoreMedia.js
@@ -23,8 +23,8 @@ class StoreMedia extends Component {
           <h4>Store Video</h4>
           <div className="line"></div>
           <div className="store-media store-video">
-            <div className="col col-md-12">
-              <iframe width="100%" height="400px" src="https://www.youtube.com/embed/q4TavxC2eVI" frameBorder="0" allowFullScreen></iframe>
+            <div className="col col-md-12 col-sm-12 col-xs-12">
+              <iframe width="100%" height="100%" src="https://www.youtube.com/embed/q4TavxC2eVI" frameBorder="0" allowFullScreen></iframe>
             </div>
           </div>
         </div>
