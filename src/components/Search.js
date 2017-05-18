71356373de63e4faf7f906d00110971b5d26878f
shrestha-bikash <bikash_stha637@hotmail.com>
2017-05-18 10:28:24 +0545

    refactored search page

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,11 +2,12 @@
  * Created by prakash on 8/29/2016.
  */
 import React, { Component } from 'react';
-import { Link } from 'react-router'
 import { connect } from 'react-redux';
+import  isEmpty  from 'lodash/isEmpty';
+
 import Filter from './Filter'
 import Deals from './Deals'
-import { sortBy, orderBy } from 'lodash';
+import StoreList from './store/StoreList';
 import ContentSearchBox from './common/ContentSearchBox';
 import { getMainCategories } from '../actions/infiniaAction';
 import {getStoresList, filterByLocation} from '../actions/storeActions';
@@ -17,33 +18,13 @@ class Search extends Component {
         this.state = {
           registered: false,
           list: false,
-          place: 'Dubai',
           class: '',
-          loc: 'active-color',
-          cat: 'passive-color',
           isShownDeals: false,
-
         }
     }
 
     componentDidMount(){
       this.props.getMainCategories();
-
-      setTimeout(()=> {
-        if(this.props.categories){
-          let category = this.props.categories.filter((cat) => {
-            return cat.category_name == this.props.routeParams.category
-          });
-          this.props.getStoresList(category[0].slug);
-        }
-      },400);
-
-
-    }
-    selectLocation(){
-        let val = document.getElementsByClassName('selectLocation');
-        console.log(val[0].value);
-        this.props.dispatch(filterByLocation(val[0].value))
     }
 
     showFilter() {
@@ -60,9 +41,8 @@ class Search extends Component {
         filter.style.display = 'block'
       }
 
-
-
     }
+
     showDeals() {
       let deals = document.getElementById('dealSection');
 
@@ -74,63 +54,37 @@ class Search extends Component {
 
       }
 
-      // this.setState(prevState => ({
-      //   isShownDeals: !prevState.isShownDeals
-      // }));
-
     }
 
     render() {
-        let {stores} = this.props;
-        console.log('in store page',stores);
+      let {categories} = this.props;
+      console.log("main categories in search", categories);
+
         return (<div className="mycontainer">
                     <div className="main-content">
-                        <div id="filterSection" className="filter-section">
-                          <Filter props={this.props} />
-                        </div>
-                        <div className="card center-content">
-                            <div className="content-title">
-                                <h2><span className="fa fa-home"/> Stores</h2>
-                                <ContentSearchBox/>
+                      <div id="filterSection" className="filter-section">
+                        <Filter props={this.props} />
+                      </div>
+                      <div className="card center-content">
+                        <div className="content-title">
+                            <h2><span className="fa fa-home"/> Stores</h2>
+                            <ContentSearchBox/>
 
-                                <div className="for-mobile-view">
-                                  <span className="fa fa-filter" onClick={()=> this.showFilter()}/>
-                                  <span className="fa fa-bolt" onClick={()=> this.showDeals()}/>
-                                </div>
-                            </div>
-
-                            <div className="all-stores">
-                                <div className="container store-sec">
-                                  <div className="row">
-                                  {stores.length>0?stores.map(store=>
-                                      <div key={store.id} className="col-md-4 col-sm-6">
-                                          <Link to={store.registered ?
-                                            { pathname: `/store/${store.display_name}`, query: { id: store.id } }
-                                            :
-                                            { pathname: `/${store.display_name}/profile`, query: { storeId: store.id } }
-
-                                            } >
-                                              <div className="thumbnail">
-                                                  <div className="ribbon"><span className={store.status == "online"? "fa fa-circle opn": "fa fa-circle clo"}> </span></div>
-                                                  <img src={store.image} />
-                                                  <div className="caption">
-                                                      <h4>{store.display_name}</h4>
-                                                      <p>Location: {store.country}, {store.state}</p>
-                                                      <p>Min-Order: {store.minimum_buy}{store.currency}</p>
-                                                      <p>Category: {store.categorysecond.map((item)=>(<span key={item.id}>{item.category_name}, </span>))}</p>
-
-                                                  </div>
-                                              </div>
-                                          </Link>
-                                    </div>
-                                  ):<h2 className="col-md-12">Sorry!!! <br/>There is no Store to show.</h2>}
-
-                                  </div>
-
-                                </div>
-
+                            <div className="for-mobile-view">
+                              <span className="fa fa-filter" onClick={()=> this.showFilter()}/>
+                              <span className="fa fa-bolt" onClick={()=> this.showDeals()}/>
                             </div>
                         </div>
+                        {
+                          !isEmpty(categories) &&
+                          <StoreList
+                            getStoresList={this.props.getStoresList}
+                            categories={categories}
+                            currentCategory={this.props.routeParams.category}
+                          />
+                        }
+
+                      </div>
                       <div id="dealSection" className="deals-section">
                         <Deals />
                       </div>
@@ -143,7 +97,6 @@ class Search extends Component {
 function mapStateToProps(state) {
     return{
       categories: state.InfiniaStores.categories,
-      stores: state.stores.stores
     }
 
 }
